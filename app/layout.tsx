6ee896d90bdf0a19066bdd1195426fa51dda0e1a
Amir A <mcattar@gmail.com>
2023-04-28 22:00:26 +0300

    Apply suggestions from code review

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 
 import Nav from './nav';
-import { Analytics } from '@vercel/analytics/react';
+import AnalyticsWrapper from './analytics';
 import Toast from './toast';
 import { Suspense } from 'react';
 
@@ -24,7 +24,7 @@ export default async function RootLayout({
           <Nav />
         </Suspense>
         {children}
-        <Analytics />
+        <AnalyticsWrapper />
         <Toast />
       </body>
     </html>
